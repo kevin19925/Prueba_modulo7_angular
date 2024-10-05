a93cf7c0ab3c7e3458819246cac81b3e747ee5ad
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-10-05 15:11:17 -0500

    Service

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Project } from '../../model/Project';
+import { ProjectService } from '../../services/project.service.service';
+import { CustomerViewComponent } from '../customer-view/customer-view.component';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [
+    CustomerViewComponent
+  ],
   templateUrl: './project.component.html',
 })
 export class ProjectComponent {
-
+  project!: Project;
+  constructor(private service: ProjectService) { }
+  ngOnInit(): void {
+    this.project = this.service.getProject();
+  }
 }
